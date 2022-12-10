ee200e190474d49c9c3db64827cd1a77d8b91dc7
edi ekeng <86800363+Edi360@users.noreply.github.com>
2022-12-10 12:28:20 -0500

    Add files via upload

--- Book_Store_database_initializer.sql
+++ Book_Store_database_initializer.sql
@@ -1,14 +1,24 @@
+CREATE TABLE IF NOT EXISTS PUBLISHERS(
+	Publisher_number INT PRIMARY KEY,
+	P_name VARCHAR(40),
+	Address SMALLINT,
+	Email_address VARCHAR(40),
+	Bank_account SMALLINT
+);
+
+
 CREATE TABLE IF NOT EXISTS Books(
 	ISBN INT PRIMARY KEY,
 	B_name VARCHAR(40),
 	author VARCHAR(40),
-	genre VARCHAR(15),
-	publisher_numbeer INT,
+	publisher_number INT,
 	number_of_pages SMALLINT,
 	price SMALLINT,
 	number_in_stock SMALLINT,
 	publisher_sale_percentage SMALLINT,
-	date_published DATE
+	date_published DATE,
+	FOREIGN KEY (publisher_number)
+		REFERENCES Publishers (publisher_number)
 	
 );
 
@@ -23,20 +33,13 @@ CREATE TABLE IF NOT EXISTS Users(
 	Billing_address INT,
 	address INT,
 	U_password VARCHAR(40),
-    Admin_privilege BOOLEAN,
+	Admin_privilege BOOLEAN,
 	FOREIGN KEY (Billing_address)
 		REFERENCES Address (A_ID),
 	FOREIGN KEY (address)
 		REFERENCES Address (A_ID)
 );
 
-CREATE TABLE IF NOT EXISTS PUBLISHERS(
-	Publisher_number INT PRIMARY KEY,
-	P_name VARCHAR(40),
-	Address SMALLINT,
-	Email_address VARCHAR(40),
-	Bank_account SMALLINT
-);
 
 CREATE TABLE IF NOT EXISTS Orders(
 	Order_number INT PRIMARY KEY,
@@ -78,6 +81,21 @@ CREATE TABLE IF NOT EXISTS CART (
 		REFERENCES Users (U_ID),
 	FOREIGN KEY (Order_number)
 		REFERENCES Orders (Order_number)
+);
+
+CREATE TABLE IF NOT EXISTS Genre(
+	G_ID INT PRIMARY KEY,
+	G_name VARCHAR(40)
+);
+
+CREATE TABLE IF NOT EXISTS Book_Genre(
+	G_ID INT,
+	ISBN INT,
+	PRIMARY KEY (G_ID,ISBN),
+	FOREIGN KEY (G_ID)
+		REFERENCES Genre (G_ID),
+	FOREIGN KEY (ISBN)
+		REFERENCES Books (ISBN)
 );
 
 
