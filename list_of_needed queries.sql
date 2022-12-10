3690fcc2cde7f7f2b32381b3174811c6a48dd011
edi360 <ediabasiekeng@gmail.com>
2022-12-10 17:18:32 -0500

    serial

--- list_of_needed queries.sql
+++ list_of_needed queries.sql
@@ -1,15 +1,23 @@
 -- Quary for getting a users user name and password
 SELECT U_name,U_password
 FROM users
-WHERE U_name='owner' AND u_password='admin'
+WHERE U_name='owner'
 
 --Signup: adds a user and their info to the data base
     --users addresses
     INSERT INTO address(address)
-VALUES
-	('The stores address');
+    VALUES
+	('mozaihas house');
     --users info
     INSERT INTO users(U_name,billing_address,address,U_password,Admin_privilege)
-    VALUES ((SELECT count(u_id)
-            FROM users
-            ),'mozaiha',2,2,'mo',FALSE);
+    VALUES ('mozaiha',(SELECT a_id
+            	FROM address
+				WHERE address='mozaihas billing address'  
+            ),(SELECT a_id
+            	FROM address
+				WHERE address='mozaihas shipping address'  
+            ),'mo',FALSE);
+    
+--Searching for books
+    --by ISBN
+    
