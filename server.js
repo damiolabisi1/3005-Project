5f8cd43dfd2a462f72d2b858c57f43b26297b8da
moissmart1 <coolandmo@gmail.com>
2022-12-10 15:51:06 -0500

    changed location

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const config = require('./config.js');
 
+const PORT = process.env.PORT || 8000;
+
+const fridgesRouter = require("./library-router.js");
+// const searchRouter = require("./search-router.js");
+
+let db;
+app.locals.db = db;
+
+// MIDDLEWARE
+app.use(express.static(""));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use((req,_,next)=> {
+    console.log(`${req.method}: ${req.url}`);
+    if (Object.keys(req.body).length > 0){
+        console.log('Body:');
+        console.log(req.body);
+    }
+    next();
+});
+
+//Mount the fridge router to the path /fridges
+//All requests starting with /fridges will be forwarded to this router
+app.use("/fridges", fridgesRouter);
+app.use("/search",searchRouter);
+app.post("/items", addItem2Collect);
 
 
 config.connect((err)=>{
