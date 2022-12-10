cb5426d98fd86cdf3cd71aa0f8375b8b9972c4d2
Damilola <damiolabisi1@gmail.com>
2022-12-10 17:35:31 -0500

    func js created

---
+++ public/func.js
@@ -0,0 +1,40 @@
+function login(){
+    let Username = document.getElementById("Username").value;
+	let Password = document.getElementById("Password").value;
+
+    const Body = {Username,Password}
+	let req = new XMLHttpRequest();
+
+    req.onreadystatechange = function () {
+        if (this.readyState==4 && this.status==200) {
+            alert("Succesfully Logged-In.\n Redirecting to home page.\n");
+            let data = JSON.parse(this.responseText);
+                window.location = "/users/"+data;
+        }
+        else if(this.readyState==4 && this.status==401) {
+            if (this.responseText == "Invalid password"){
+                document.getElementById("Err").innerHTML = "Incorrect Password";
+            }
+            else{
+                document.getElementById("Err").innerHTML = "";
+            }
+            if (this.responseText == "Invalid username"){
+            document.getElementById("UserErr").innerHTML = "Incorrect Username";
+            }
+            else{
+                document.getElementById("UserErr").innerHTML = "";
+            }
+
+            if (this.responseText == "Invalid credentials" && "Invalid credentials pass"){
+                document.getElementById("Err").innerHTML = "Incorrect Password";
+                document.getElementById("UserErr").innerHTML = "Incorrect Username";
+            }
+            
+        }
+        };
+        req.open("POST", "/login", true);
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send(JSON.stringify(Body));
+    }
+
+    
