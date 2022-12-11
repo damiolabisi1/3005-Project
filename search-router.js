463d79b7a8c374d1aeb8ad3195d414d4aebddde4
moissmart1 <coolandmo@gmail.com>
2022-12-11 07:39:14 -0500

    first test of search back end

--- search-router.js
+++ search-router.js
@@ -6,20 +6,48 @@ let router = express.Router();
 const config = require('./config.js');
 
 app.use(express.json()); // body-parser middleware
+router.post('/', (request,response) => {
+    let text = request.body.text;
+    let type = request.body.type.toLowerCase();
+        //by ISBN
+        if(type == isbn )
+        config.query("SELECT * FROM books WHERE ISBN="+search_text, (err,res)=>{
+            if (err) throw err;
+            if(res.rows.length > 0){
+                response.send(JSON.stringify(res.rows));
+                return;
+            }
+        });
 
+        //by author
+        else if(type == author )
+        config.query("SELECT * FROM books WHERE author=\'"+search_text+"\'", (err,res)=>{
+            if (err) throw err;
+            if(res.rows.length > 0){
+                response.send(JSON.stringify(res.rows));
+                return;
+            }
+        });
+    
+        else if(type == name )
+        config.query("SELECT * FROM books WHERE B_name=\'"+search_text+"\'", (err,res)=>{
+            if (err) throw err;
+            if(res.rows.length > 0){
+                response.send(JSON.stringify(res.rows));
+                return;
+            }
+        });
+    
+        else if(type == gnere )
+        config.query("SELECT books.ISBN,books.B_name,books.author,books.publisher_number,books.number_of_pages,books.price,books.number_in_stock,books.publisher_sale_percentage,books.date_published FROM genre NATURAL JOIN book_genre NATURAL JOIN books WHERE G_name = \'"+search_text+"\'", (err,res)=>{
+            if (err) throw err;
+            if(res.rows.length > 0){
+                response.send(JSON.stringify(res.rows));
+                return;
+            }
+        });
 
-router.get("/", (req,res)=> {
-    res.format({
-		'text/html': ()=> {
-			res.set('Content-Type', 'text/html');
-			res.sendFile(path.join(__dirname,'public','search.html'),(err) =>{
-				if(err) res.status(500).send('500 Server error');
-			});
-		},
-        'default' : ()=> {
-            res.status(406).send('Not acceptable');
-        }
-    })
 });
+
 
 module.exports = router;
