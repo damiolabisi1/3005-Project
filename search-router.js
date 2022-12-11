e650c6c2da0663885739c48a05e036d424d51fa1
moissmart1 <coolandmo@gmail.com>
2022-12-10 19:35:52 -0500

    password and username now working even if username is wrong

--- search-router.js
+++ search-router.js
@@ -7,4 +7,19 @@ const config = require('./config.js');
 
 app.use(express.json()); // body-parser middleware
 
+
+// router.get(["/","/home"], (req,res)=> {
+//     res.format({
+// 		'text/html': ()=> {
+// 			res.set('Content-Type', 'text/html');
+// 			res.sendFile(path.join(__dirname,'public','home.html'),(err) =>{
+// 				if(err) res.status(500).send('500 Server error');
+// 			});
+// 		},
+//         'default' : ()=> {
+//             res.status(406).send('Not acceptable');
+//         }
+//     })
+// });
+
 module.exports = router;
