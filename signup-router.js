118a95d15f4b03e495a68696184479e332402686
moissmart1 <coolandmo@gmail.com>
2022-12-10 20:10:55 -0500

    working login

--- signup-router.js
+++ signup-router.js
@@ -7,4 +7,39 @@ let router = express.Router();
 
 app.use(express.json()); // body-parser middleware
 
+router.post(["/signup"], (request,response) => {
+    let username = request.body.username;
+    let password = request.body.password;
+
+    if(username == ""){
+        response.status(401).send("Not valid user");
+    }
+    else if(request.body.password == ""){
+        response.status(401).send("Not valid");
+    }
+    
+    else{
+        config.query('SELECT U_id,U_name,U_password From users Where U_name=\''+username+"\'", (err,res)=>{
+            console.log(err,res)
+            if(err) throw err;
+
+            if(res.rows.length == 0){
+                db.collection("users").insertOne({username : username, password :password, privacy: privacy})
+                    db.collection("users").findOne({username : username},function(err, result){  
+                        if(err) throw err;
+
+                        response.status(200).send(result._id);
+                    })
+                }
+            else{
+                response.status(401).send("Username taken");
+                return;
+        }
+        
+    })
+}
+
+});
+
+
 module.exports = router;
