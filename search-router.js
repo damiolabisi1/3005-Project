518eed784820e215b9e56558407f959e8f0983b1
moissmart1 <coolandmo@gmail.com>
2022-12-10 22:22:44 -0500

    same

--- search-router.js
+++ search-router.js
@@ -8,18 +8,18 @@ const config = require('./config.js');
 app.use(express.json()); // body-parser middleware
 
 
-// router.get(["/","/home"], (req,res)=> {
-//     res.format({
-// 		'text/html': ()=> {
-// 			res.set('Content-Type', 'text/html');
-// 			res.sendFile(path.join(__dirname,'public','home.html'),(err) =>{
-// 				if(err) res.status(500).send('500 Server error');
-// 			});
-// 		},
-//         'default' : ()=> {
-//             res.status(406).send('Not acceptable');
-//         }
-//     })
-// });
+router.get(["/","/search"], (req,res)=> {
+    res.format({
+		'text/html': ()=> {
+			res.set('Content-Type', 'text/html');
+			res.sendFile(path.join(__dirname,'public','search.html'),(err) =>{
+				if(err) res.status(500).send('500 Server error');
+			});
+		},
+        'default' : ()=> {
+            res.status(406).send('Not acceptable');
+        }
+    })
+});
 
 module.exports = router;
--- signup-router.js
+++ signup-router.js
@@ -10,6 +10,8 @@ app.use(express.json()); // body-parser middleware
 router.post(["/signup"], (request,response) => {
     let username = request.body.username;
     let password = request.body.password;
+    let user_address = request.body.address;
+    let user_billing_address = request.billing_address;
 
     if(username == ""){
         response.status(401).send("Not valid user");
@@ -23,12 +25,27 @@ router.post(["/signup"], (request,response) => {
             console.log(err,res)
             if(err) throw err;
 
-            if(res.rows.length == 0){
-                config.query("INSERT INTO address(address) VALUES (\'"+user_address+"\')", (err,res)=>{
+            else if(res.rows.length == 0){
+                config.query("SELECT A_ID FROM address WHERE address=\'"+user_address+"\'", (err,resulter)=>{
                     if(err) throw err;
+                    else if(resulter.rows.length == 0){
+                            config.query("INSERT INTO address(address) VALUES (\'"+user_address+"\')", (err,result)=>{
+                                if(err) throw err;
+                            })
+                            config.query("INSERT INTO address(address) VALUES (\'"+user_address+"\')", (err,result)=>{
+                                if(err) throw err;
+                            })
+                            config.query("INSERT INTO users(U_name,billing_address,address,U_password,Admin_privilege) VALUES (\'"+username+"\',(SELECT a_id FROM address WHERE address=\'"+user_billing_address+"\'),(SELECT a_id FROM address WHERE address=\'"+user_address+"\'),\'"+user_password+"\',FALSE)", (err,res)=>{
+                                if(err) throw err;
+                                console.log(res)
+                            });
+                        }
                     else{
+                        config.query("INSERT INTO users(U_name,billing_address,address,U_password,Admin_privilege) VALUES (\'"+username+"\',\'"+user_billing_address+"\',\'"+resulter.row[1].address_id+"\',\'"+password+"\',FALSE)", (err,res)=>{
+                            if(err) throw err;
+                        });
+                    }
                         
-                    };
                 });
                 }
             else{
